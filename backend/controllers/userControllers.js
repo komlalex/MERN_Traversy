598a27b370f8a1ae7087ebb39a787e85e3ae8d6c
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-11-10 16:45:37 -0800

    backend completed

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,11 +1,28 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+
+
 // @desc Auth user/set token
 // @route POST api/users/auth 
 // @access public
-
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Login User"})
+    const {email, password} = req.body;
+    const user = await User.findOne({email:email});
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400);
+        throw new Error("Invalid Email or Password");
+    }
+    
+    
 })
 
 // @desc Register new user
@@ -34,6 +51,7 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 
     if (user) {
+        generateToken(res, user._id)
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -50,29 +68,51 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST api/users/logout  
 // @access public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Logout User"})
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({message: "Logged out"})
 })
 
 // @desc Get user profile 
 // @route GET api/users/profile
 // @access private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Get user profile"})
+    const user = {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    }
+    res.status(200).json(user);
 })
 
 // @desc Update user profile
 // @route PUT api/users/profile
 // @access private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Update user"})
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+        const updatedUser = await user.save();
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email
+        })
+        
+    } else {
+        res.status(404);
+        throw new Error("User not found")
+    }
 })
 
-// @desc Delete user
-// @route DELETE api/users
-// @access private
-const deleteUserProfile = asyncHandler(async(req, res) => {
-    res.status(200).json({message: "Delete User"})
-})
 
 
 export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
 connectDB()
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
 app.use(cors());
 
